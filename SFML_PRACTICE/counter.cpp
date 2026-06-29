270897053ea94978cd80afad620927c5fe01cff2
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-06-29 15:12:15 +0300

    added handleClick method to the buttons

--- SFML_PRACTICE/counter.cpp
+++ SFML_PRACTICE/counter.cpp
@@ -1,5 +1,6 @@
 #include<SFML/Graphics.hpp>
 #include <iostream>
+#include <functional>
 
 class Mouse
 {
@@ -42,6 +43,7 @@ class Button : public sf::Drawable, public sf::Transformable
 {
 	sf::RectangleShape m_shape;
 	sf::Text m_text;
+	std::function<void()> m_onClick;
 
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override
 	{
@@ -74,6 +76,27 @@ public:
 	{
 		return this->m_shape.getSize();
 	}
+
+	bool contains(sf::Vector2f point) const
+	{
+		sf::Transform inverse = getTransform().getInverse();
+		sf::Vector2f localPoint = inverse.transformPoint(point);
+
+		return m_shape.getGlobalBounds().contains(localPoint);
+	}
+
+	void setOnClick(std::function<void()> callback)
+	{
+		m_onClick = callback;
+	}
+
+	void handleClick(sf::Vector2f mousePosition)
+	{
+		if (contains(mousePosition) && m_onClick)
+		{
+			m_onClick();
+		}
+	}
 };
 
 int counter()
@@ -86,10 +109,39 @@ int counter()
 	}
 	// create mouse object
 	Mouse mouse;
-	Button button(font, "Hello");
-	button.setOrigin(button.getSize() / 2.f);
-	button.setPosition({ 200, 250 });
 
+	// create button objects
+	Button incrementButton(font, "+");
+	incrementButton.setPosition({ 100, 250 });
+
+	Button decrementButton(font, "-");
+	decrementButton.setPosition({ 220, 250 });
+
+	Button resetButton(font, "reset", { 220.f, 50.f });
+	resetButton.setPosition({ 100, 350 });
+
+	// create counter text
+	int count = 0;
+	sf::Text counterText(font);
+	counterText.setString(std::to_string(count));
+	auto counterTextBounds = counterText.getLocalBounds();
+	counterText.setOrigin({counterTextBounds.size.x / 2.f , counterTextBounds.size.y / 2.f});
+	counterText.setPosition({200.f, 150.f});
+
+	incrementButton.setOnClick([&]() {
+		count++;
+		counterText.setString(std::to_string(count));
+		});
+
+	decrementButton.setOnClick([&]() {
+		count--;
+		counterText.setString(std::to_string(count));
+		});
+
+	resetButton.setOnClick([&]() {
+		count = 0;
+		counterText.setString(std::to_string(count));
+		});
 
 	// create window
 	sf::RenderWindow window(sf::VideoMode({ 400, 500 }), "Counter");
@@ -97,7 +149,7 @@ int counter()
 	while (window.isOpen())
 	{
 		// handle events
-		if (std::optional event = window.pollEvent())
+		while (std::optional event = window.pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
 			{
@@ -105,19 +157,25 @@ int counter()
 			}
 		}
 		mouse.Update();
+		sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+		sf::Vector2f mousePosF{
+		    static_cast<float>(mousePos.x),
+		    static_cast<float>(mousePos.y)
+		};
 
 		if (mouse.onPress)
 		{
-			std::cout << "mouse pressed\n";
-		}
-		if (mouse.onRelease)
-		{
-			std::cout << "mouse released\n";
+			incrementButton.handleClick(mousePosF);
+			decrementButton.handleClick(mousePosF);
+			resetButton.handleClick(mousePosF);
 		}
 
 		window.clear(sf::Color::Black);
 
-		window.draw(button);
+		window.draw(counterText);
+		window.draw(incrementButton);
+		window.draw(decrementButton);
+		window.draw(resetButton);
 
 		window.display();
 	}
